443f9758a0becba5257405f8d3e2b9e0289de656
Mohammed Juzer <mohammedjuzer10@gmail.com>
2024-02-07 23:40:25 +0530

    Optimization of code and adding of confetti

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ let score = 20;
 let highScore = 0;
 console.log(secretNumber);
 
+ // Creating a function for reusability
+ function displayMessage(message) {
+  document.querySelector(".message").textContent = message;
+}
+
 document.querySelector(".check").addEventListener("click", function () {
   const userInput = Number(document.querySelector(".guess").value);
-
   if (!userInput) {
-    document.querySelector(".message").textContent =
-      "Please enter a valid number!!";
+    displayMessage("Please enter a valid number!!");
   } else if (userInput > 20) {
     alert("The number is between 1 - 20 only!");
   } else if (secretNumber === userInput) {
     document.querySelector(".number").textContent = userInput;
-    document.querySelector(".message").textContent = "You WIN !!";
+    displayMessage("You WIN !!");
     document.querySelector("body").style.backgroundColor = "green";
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti();
 
     // Update New high Score
     if (score >= highScore) {
@@ -22,20 +27,26 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".highscore").textContent = highScore;
     }
   } else if (secretNumber !== userInput) {
-    score--;
-    document.querySelector(".score").textContent = score;
-    document.querySelector(".check").textContent = "Try Again.";
-    if (userInput < secretNumber) {
-      document.querySelector(".message").textContent = "Too Low";
+    if (score > 1) {
+      score--;
+      document.querySelector(".score").textContent = score;
+      displayMessage("Try Again.");
+      // if (userInput < secretNumber) {
+      //   displayMessage("Too Low");
+      // } else {
+      //   displayMessage("Too High");
+      // }
+
+      displayMessage(userInput < secretNumber ? "Too Low" : "Too High");
     } else {
-      document.querySelector(".message").textContent = "Too High";
+      displayMessage("Sorry...You Lose!");
     }
   }
 });
 
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector("body").style.backgroundColor = "#292928";
